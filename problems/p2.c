a104917adac615521360e4567d50934d6d9f70d6
vishakh k t <vishakhkt.2003@gmail.com>
2025-07-16 10:56:42 +0530

    problems added

--- problems/p2.c
+++ problems/p2.c
@@ -2,8 +2,8 @@
 
 int main(){
     for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
-            printf("%d ", i);
+        for(int j=1;j<=i;j++){
+            printf("%d ",j);
         }
         printf("\n");
     }
